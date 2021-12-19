0eb0f321297d2aa708b4897e0aa53dcb3c27f4a9
Анаста Михайлова <nastyamihajlova13@gmail.com>
2021-12-19 13:01:59 +0300

    проверка действий

---
+++ Programma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace laboratory_work;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		Console.WriteLine("Привет!!!");
+	}
+}
